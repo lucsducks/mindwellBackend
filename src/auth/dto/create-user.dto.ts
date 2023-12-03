7e9a461515f71586528f97645c5675fa16f674f5
lucsducks <2021460003@unheval.pe>
2023-12-03 16:55:55 -0500

    modificado el auth register

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -16,8 +16,8 @@ export class CreateUserDto {
     @IsString()
     @MinLength(6)
     @MaxLength(60)
-    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/, {
-        message: 'the password must have a Uppercase,lowercase and number',
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\S]{6,}$/, {
+        message: 'the password must have an uppercase, a lowercase, a number, and optionally special characters',
     })
     password: string;
 
